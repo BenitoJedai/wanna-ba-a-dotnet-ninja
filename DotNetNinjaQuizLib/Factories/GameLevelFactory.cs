a61993cbe60c9b99927daf3a1bc2d6e9e6c6af7c
torbjorn.maro <torbjorn.maro@0ef3c780-9af9-11de-96bd-0376b6bc6ad2>
2009-09-11 05:22:57 +0000

    Added font scaling to Questions and Answers. Also changed the question levels from dollars to geek titles.

--- DotNetNinjaQuizLib/Factories/GameLevelFactory.cs
+++ DotNetNinjaQuizLib/Factories/GameLevelFactory.cs
@@ -18,31 +18,31 @@ namespace DotNetNinjaQuizLib.Factories
             list.Add(
                 1, new GameLevel()
             {
-                Label = "$100",
+                Label = "Newbie",
                 DifficultySelector = new DifficultyDecider_Easy(),
             });
             list.Add(
                 2, new GameLevel()
                 {
-                    Label = "$200",
+                    Label = "Script Kiddie",
                     DifficultySelector = new DifficultyDecider_Easy(),
                 });
             list.Add(
                 3, new GameLevel()
                 {
-                    Label = "$300",
+                    Label = "Code Monkey",
                     DifficultySelector = new DifficultyDecider_EasyToMedium(),
                 });
             list.Add(
                 4, new GameLevel()
                 {
-                    Label = "$500",
+                    Label = "Trainee",
                     DifficultySelector = new DifficultyDecider_EasyToMedium(),
                 });
             list.Add(
                 5, new GameLevel()
                 {
-                    Label = "$1,000",
+                    Label = "Junior",
                     DifficultySelector = new DifficultyDecider_EasyToMedium(),
                 });
             #endregion
@@ -50,31 +50,31 @@ namespace DotNetNinjaQuizLib.Factories
             list.Add(
                 6, new GameLevel()
                 {
-                    Label = "$2,000",
+                    Label = "Apprentice I",
                     DifficultySelector = new DifficultyDecider_Medium(),
                 });
             list.Add(
                 7, new GameLevel()
                 {
-                    Label = "$4,000",
+                    Label = "Apprentice II",
                     DifficultySelector = new DifficultyDecider_Medium(),
                 });
             list.Add(
                 8, new GameLevel()
                 {
-                    Label = "$8,000",
+                    Label = "Journeyman I",
                     DifficultySelector = new DifficultyDecider_Medium(),
                 });
             list.Add(
                 9, new GameLevel()
                 {
-                    Label = "$16,000",
+                    Label = "Journeyman II",
                     DifficultySelector = new DifficultyDecider_MediumToHard(),
                 });
             list.Add(
                 10, new GameLevel()
                 {
-                    Label = "$32,000",
+                    Label = "Professional",
                     DifficultySelector = new DifficultyDecider_MediumToHard(),
                 });
             #endregion
@@ -82,31 +82,31 @@ namespace DotNetNinjaQuizLib.Factories
             list.Add(
                 11, new GameLevel()
                 {
-                    Label = "$64,000",
+                    Label = "Mentor",
                     DifficultySelector = new DifficultyDecider_MediumToHard(),
                 });
             list.Add(
                 12, new GameLevel()
                 {
-                    Label = "$125,000",
+                    Label = "Syntax Monk",
                     DifficultySelector = new DifficultyDecider_MediumToHard(),
                 });
             list.Add(
                 13, new GameLevel()
                 {
-                    Label = "$250,000",
+                    Label = "Black Belt",
                     DifficultySelector = new DifficultyDecider_Difficult(),
                 });
             list.Add(
                 14, new GameLevel()
                 {
-                    Label = "$500,000",
+                    Label = ".Net Guru",
                     DifficultySelector = new DifficultyDecider_Difficult(),
                 });
             list.Add(
                 15, new GameLevel()
                 {
-                    Label = "$1 MILLION",
+                    Label = ".NET NINJA",
                     DifficultySelector = new DifficultyDecider_Difficult(),
                 });
             #endregion
